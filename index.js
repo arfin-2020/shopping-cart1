1b57f13be4642f6659f34c0dde56932c0221d105
unknown <arfinchowdhuryarif7@gmail.com>
2023-06-05 07:13:09 +0600

    finally this project is done.

--- index.js
+++ index.js
@@ -1,39 +1,97 @@
-
-
-const capturingInputFieldValueById = (elementId)=>{
-    const InputElement = document.getElementById(elementId);
-    const inputFieldNumberValue = Number(InputElement.value);
-    return inputFieldNumberValue;
+const capturingInputFieldValueById = (elementId) => {
+    const inputElement = document.getElementById(elementId);
+    const inputFieldparseFloatValue = Number(inputElement.value);
+    return inputFieldparseFloatValue;
 }
 
-const totalCalculationOfIphoneByID = (elementId,elementId2, totalCalculation,countdownValueByClicking)=>{
-    // iPhone price calculation
-    const iphonePriceElement = document.getElementById(elementId);
-    const iphoneInputElement = document.getElementById(elementId2);
-    // iphoneInputElement.value = countdownValueByClicking;
-    iphoneInputElement.value = countdownValueByClicking;
-    
-    // iPhone price sumetion by countdown value;
-    const totalSumationOfIphonePrice = totalCalculation;
-    iphonePriceElement.innerText = totalSumationOfIphonePrice;
+const innerTextValueUsingID = (elementId)=>{
+    const capturingElement = document.getElementById(elementId);
+    const PreviousPrice = Number(capturingElement.innerText);
+    return PreviousPrice;
+}
+// Here I implemented Subtotal, Tax, Total 
+
+const totalCalculationOfSubtotalTaxTotalUsingID = (iphonePrice, iphoneCasePrice, subtotalId, taxId, totalId) => {
+  
+    const subTotalElement = document.getElementById(subtotalId);
+    const iphoneTotalPrice = innerTextValueUsingID(iphonePrice);
+    const iphoneCaseTotalPrice = innerTextValueUsingID(iphoneCasePrice);
+    const subTotalCalculation = Number(iphoneTotalPrice + iphoneCaseTotalPrice);
+    subTotalElement.innerText = subTotalCalculation;
+    const taxElement = document.getElementById(taxId);
+
+    // tax calculation
+    const taxWillBe = parseFloat((subTotalCalculation * 5) / 100);
+    taxElement.innerText = taxWillBe;
+
+    // total Calculation
+    const totalElement = document.getElementById(totalId);
+    const totalSum = (subTotalCalculation + taxWillBe);
+    totalElement.innerText = totalSum.toFixed(2);;
 }
 
-// Here I implemented Plus Button Calculation with iphone price.
-document.getElementById('btn-plus-iphone').addEventListener("click", function(){
+const updateProductTotal = (price, inputFieldId, subtotalId, taxId, totalId, workFor, productName) => {
+    const inputFieldElement = document.getElementById(inputFieldId);
+    const productPriceElement = document.getElementById(price);
+    const inputFieldValue = capturingInputFieldValueById(inputFieldId);
+    inputFieldElement.value = inputFieldElement.value
+    let countdownValueByClicking;
+    let totalSumationOfIphonePrice;
+    let totalSumationOfCasePrice;
+    if (productName === 'iphone' && workFor === 'plus') {
 
-    const inputFieldValue = capturingInputFieldValueById('iphone-inputField');
-    const countdownValueByClicking = inputFieldValue + 1; 
-    
+        countdownValueByClicking = inputFieldValue + 1;
+        inputFieldElement.value = countdownValueByClicking;
+        // Product price calculation
 
-    // iPhone price calculation + iPhone price sumetion by countdown value;
-    const iphonePrice = 1219;
-    const totalSumationOfIphonePrice = (countdownValueByClicking * iphonePrice);
-    totalCalculationOfIphoneByID('iphone-price',"iphone-inputField",totalSumationOfIphonePrice,countdownValueByClicking)
-})
+        const iphonePrice = 1219;
+        totalSumationOfIphonePrice = Number(countdownValueByClicking * iphonePrice);
+    } else if(productName === 'iphone' && workFor === 'minus') {
+        countdownValueByClicking = inputFieldValue - 1;
+        inputFieldElement.value = countdownValueByClicking;
+        // Product price calculation
 
-// Here I implemented minus Button Calculation with iphone price. 
-document.getElementById('btn-minus-iphone').addEventListener("click", function(){
-    // console.log('He click me bro.')
-    const iphoneInputElement = document.getElementById()
+        const iphonePrice = 1219;
+        totalSumationOfIphonePrice = Number(countdownValueByClicking * iphonePrice);
+    }else if (productName === 'iphoneCase' && workFor === 'plus') {
 
-})
+        countdownValueByClicking = inputFieldValue + 1;
+        inputFieldElement.value = countdownValueByClicking;
+        // Product price calculation
+
+        const iphonePrice = 59;
+        totalSumationOfCasePrice = Number(countdownValueByClicking * iphonePrice);
+    } else if(productName === 'iphoneCase' && workFor === 'minus') {
+        countdownValueByClicking = inputFieldValue - 1;
+        inputFieldElement.value = countdownValueByClicking;
+        // Product price calculation
+
+        const iphonePrice = 59;
+        totalSumationOfCasePrice = Number(countdownValueByClicking * iphonePrice);
+    }
+
+       productPriceElement.innerText = totalSumationOfIphonePrice || totalSumationOfCasePrice;
+
+    // Update subtotal, tax, and total
+    totalCalculationOfSubtotalTaxTotalUsingID('iphone-price','iphoneCase-price', subtotalId, taxId, totalId);
+}
+
+// Plus button click handler for iPhone
+document.getElementById('btn-plus-iphone').addEventListener("click", function () {
+    updateProductTotal('iphone-price', 'iphone-inputField', 'Subtotal', 'tax', 'total', 'plus', 'iphone');
+});
+
+// Minus button click handler for iPhone
+document.getElementById('btn-minus-iphone').addEventListener("click", function () {
+    updateProductTotal('iphone-price', 'iphone-inputField', 'Subtotal', 'tax', 'total', 'minus', 'iphone');
+});
+
+// Plus button click handler for iPhone Case
+document.getElementById('btn-plus-iphoneCase').addEventListener("click", function () {
+    updateProductTotal('iphoneCase-price', 'iphoneCase-inputField', 'Subtotal', 'tax', 'total', 'plus', 'iphoneCase');
+});
+
+// Minus button click handler for iPhone Case
+document.getElementById('btn-minus-iphoneCase').addEventListener("click", function () {
+    updateProductTotal('iphoneCase-price', 'iphoneCase-inputField', 'Subtotal', 'tax', 'total', 'minus', 'iphoneCase');
+});
